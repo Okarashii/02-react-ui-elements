ad17b0631d274b693e73773b8c9adb68f38a58d1
2cev <tucev@hotmail.com>
2024-03-07 11:05:04 +0100

    Ende Modulstunde

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // O_o => states
+  const [isToggled, setToggled] = useState(true);
+  const [isChecked, setCheckBoxed] = useState(true);
+  const [inputVorname, setInputVorname] = useState('');
+  const [inputName, setInputName] = useState('');
+  const [inputRadioBtn, setRadioBtn] = useState('');
+  // O_o => fncs
+  const toggleTrueFalse = () => setToggled(!isToggled);
+  const checkBoxTrueFalse = () => setCheckBoxed(!isToggled);
+  const updateVorname = () => setInputVorname(inputVorname);
+  const updateName = () => setInputName(inputName);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Skitag Anmeldung</p>
       </header>
+
+      <form>
+        
+      <label>
+          Vorname:
+          <input type='text' value='inputVorname'
+            onChange={(e)=> {
+              setInputVorname(e.target.value);
+            }}
+          />
+      </label>
+
+      <label>
+          Name:
+          <input type='text' value='inputName'
+            onChange={(e)=> {
+              setInputName(e.target.value);
+            }}
+          />
+        </label>
+
+
+      </form>
     </div>
   );
 }
